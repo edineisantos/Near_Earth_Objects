d394755e708c8113fcab2ab3edc91789b7a3c6d9
Edinei Santos <7681342+edineisantos@users.noreply.github.com>
2023-12-25 10:25:30 -0300

    Review write.py to meet PEP 8 and PEP 257

--- write.py
+++ write.py
@@ -7,8 +7,6 @@ write the data.
 These functions are invoked by the main module with the output of the `limit`
 function and the filename supplied by the user at the command line. The file's
 extension determines which of these functions is used.
-
-You'll edit this file in Part 4.
 """
 import csv
 import json
@@ -18,12 +16,13 @@ import os
 def write_to_csv(results, filename):
     """Write an iterable of `CloseApproach` objects to a CSV file.
 
-    The precise output specification is in `README.md`. Roughly, each output row
-    corresponds to the information in a single close approach from the `results`
-    stream and its associated near-Earth object.
+    The precise output specification is in `README.md`. Roughly, each output
+    row corresponds to the information in a single close approach from the
+    `results` stream and its associated near-Earth object.
 
     :param results: An iterable of `CloseApproach` objects.
-    :param filename: A Path-like object pointing to where the data should be saved.
+    :param filename: A Path-like object pointing to where the data should be
+    saved.
     """
     fieldnames = (
         'datetime_utc', 'distance_au', 'velocity_km_s',
@@ -43,13 +42,14 @@ def write_to_csv(results, filename):
 def write_to_json(results, filename):
     """Write an iterable of `CloseApproach` objects to a JSON file.
 
-    The precise output specification is in `README.md`. Roughly, the output is a
-    list containing dictionaries, each mapping `CloseApproach` attributes to
+    The precise output specification is in `README.md`. Roughly, the output is
+    a list containing dictionaries, each mapping `CloseApproach` attributes to
     their values and the 'neo' key mapping to a dictionary of the associated
     NEO's attributes.
 
     :param results: An iterable of `CloseApproach` objects.
-    :param filename: A Path-like object pointing to where the data should be saved.
+    :param filename: A Path-like object pointing to where the data should be
+    saved.
     """
     # Ensure the data_output directory exists
     os.makedirs('data_output', exist_ok=True)
